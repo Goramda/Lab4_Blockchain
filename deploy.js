3b2dccacce92adb815c505cf3ed8e14321f90961
Goramda <goddy_2556@hotmail.com>
2019-10-03 15:27:51 +0700

    Commit2

--- deploy.js
+++ deploy.js
@@ -1,8 +1,11 @@
 const Web3 = require("web3");
-const web3 = new Web3();
-web3.setProvider(new
-web3.providers.HttpProvider('http://localhost:8545'));
+// const web3 = new Web3();
+// web3.setProvider(new
+// web3.providers.HttpProvider('http://localhost:8545'));
 const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const provider = new HDWalletProvider('word group hazard auto like bottom credit brand bamboo family ignore plug','https://rinkeby.infura.io/v3/1072171e0f0b4ae1ac03e9b72af2b329');
+const web3 = new Web3(provider);
 
 const deploy = async () => {
     accounts = await web3.eth.getAccounts();
